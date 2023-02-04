0730b926ee1e7f3e0291afe81ec76bc71e7b20a5
Olga <111655363+HronaOlha@users.noreply.github.com>
2023-02-04 22:29:49 +0200

    Зроблена задача 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
 
+stopBtn.setAttribute('disabled', true);
+
+startBtn.addEventListener('click', handleColorChange);
+stopBtn.addEventListener('click', handleStopColorChange);
+
+function handleColorChange() {
+  startBtn.setAttribute('disabled', true);
+  if (stopBtn.hasAttribute('disabled')) {
+    stopBtn.removeAttribute('disabled');
+  }
+
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function handleStopColorChange() {
+  stopBtn.setAttribute('disabled', true);
+  if (startBtn.hasAttribute('disabled')) {
+    startBtn.removeAttribute('disabled');
+  }
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
