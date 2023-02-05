845f28af4660b457257cffbac3813262011c007e
HronaOlha <111655363+HronaOlha@users.noreply.github.com>
2023-02-05 13:02:13 +0000

    Deploying to gh-pages from @ HronaOlha/goit-js-hw-09@18f6bb5e841ce715e7808a915ec1f12739f8b5f4 🚀

---
+++ 01-color-switcher.4eb1c00c.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=null;e.disabled=!0,t.addEventListener("click",(function(){t.disabled=!0,e.disabled&&(e.disabled=!1);d=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){e.disabled=!0,t.disabled&&(t.disabled=!1);clearInterval(d)}))}();
+//# sourceMappingURL=01-color-switcher.4eb1c00c.js.map
